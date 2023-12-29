44fcd5f61f970973b1ec1dab8a36914a1e2b1ced
ayaKaadanDev <ayaKaadan17@gmail.com>
2023-12-29 11:03:14 +0200

    added new tables to the DB with some column

--- LPWGH/database/migrations/2023_12_29_084419_create_products_table.php
+++ LPWGH/database/migrations/2023_12_29_084419_create_products_table.php
@@ -11,16 +11,15 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('products', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->string('profile_ing_url');
-            $table->string('whatsapp_url');
-            $table->string('facebook_url');
-            $table->rememberToken();
+            $table->double('price');
+            $table->string('description');
+            $table->date('exp_date');
+            $table->string('img_url');
+            $table->integer('quantity')->default(1);
+            $table->integer('category_id');
             $table->timestamps();
         });
     }
@@ -30,6 +29,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('products');
     }
 };
